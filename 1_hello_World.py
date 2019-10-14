8e62a8e79ee1b57aedfc3fe2ac10a421be99b0d1
dtekluva <kboysreel@gmail.com>
2019-10-14 06:22:14 +0100

    VIDEO 6 UPDATE

--- 1_hello_World.py
+++ 1_hello_World.py
@@ -1,9 +1,42 @@
 
-2 + 2
-print(2 + 2)
-print(2 + 3)
-print(2 * 5)
-print("Hello world")
-print("This is my first code")
+#2 + 2
+#print(2 + 2)
+# print(2 + 3)
+# print(2 * 5)
+# print("Hello world")
+# print("This is my first code")
 
-print("1 plus 1 is equal to ", 1+1)
+# print("1 plus 1 is equal to ", 1+1)
+## COMMENTING CODE ---> CTRL + /
+
+# console.log("i ama a boy")  ##SYNTAX ERROR 
+
+# X = 20 #Bad naming convention
+# price = 300 #good naming convention readable
+
+# total = 20 + price
+# print(total)
+
+# side_of_square = 3
+# area_of_square = side_of_square * side_of_square
+# print(area_of_square)
+
+# age = 20
+# print(age)
+
+# prefered_meal, prefered_dance, age = "Bread meal", "Moon walking", 20 # Assigning multiple variables at once
+
+# print(prefered_dance)
+# print(prefered_meal)
+# print(age)
+
+
+# name = age = job = "OOUPS MOTHING FOUND"
+# print(job)
+
+
+# True = 20
+
+# if = "hello"
+
+# CHALLENGE WRITE A PROGRAM TO COLLECT A USERS NAME AND PRINT IT (INPUT)
