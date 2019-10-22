6bc044a85600f4d322149b661672d3318ec56da7
dtekluva <kboysreel@gmail.com>
2019-10-22 02:42:18 +0100

    datatypes tut

--- datatypes.py
+++ datatypes.py
@@ -1,3 +1,129 @@
+# # DATATYPES
+
+# # INTEGERS
+# # FLOAT
+# # STRINGS
+# # BOOLEANS
+# # COMPLEX NUMBER
+
+# # + plus
+# # - minus
+# # / division
+# # * multiplication
+
+# #triangle side_a = 5, side_b = 4 find side_c - (PYTHAGORAS)
+# # side_a = input("Please enter side A : ") 
+# # side_b = input("Please enter side B : ")
+
+# # side_a = int(side_a) #CAST INPUT RESPONSES INTO INTEGERS FROM STRING FORMAT
+# # side_b = int(side_b)
+
+# # print(type(side_a))
+# # print(type(side_b))
+
+# # side_c = ???
+# # side_c = (side_a ** 2 + side_b ** 2) ** 0.5
+# # rounded_result = round(side_c, 3)
+# # print(side_c)
+# # print(rounded_result)
+
+# # print()
+# # print("Hello user please note that the")
+# # print("program outputs 0 for a leap ")
+# # print("year and any other number for a none leap year")
+# # print()
+
+# # year = int(input("Please enter a year to test : "))
+
+# # remainder = year % 4
+
+# # print(year, "--->", remainder)
+# # print(year, "is a Leap year --->", not bool(remainder))
+
+# # WRITE A PROGRAM TO TAKE A PERSONS AGE, AND FIND THE CORRSPONDING YEAR OF BIRTH AND THEN TELL IF SAID PERSON WAS BORN IN A LEAP YEAR
+
+# # comment (code ctrl + /)
+ 
+# # age = int(input("Please enter your age : "))
+
+# # year_of_birth = 2019 - age
+
+# # remainder = year_of_birth % 4
+
+# # print('Hello you were born in the year', year_of_birth)
+# # print(year_of_birth, "divided by 4 --->", remainder)
+# # print(year_of_birth, "is a Leap year --->", not bool(remainder))
+
+# # OTHER OPERATORS
+
+# #EXPONENT (**) RAISE TO POWER
+# X = 3 ** 3 # --> 27
+# print(X)
+# #FLOOR DIVISION (//) RAISE TO POWER
+# Y = 22 // 7 # --> 3
+# print(Y)
+
+# rounded = round(22/7, 4)#approximation using the round function
+# print(rounded)
+
+
+# #OPERATORS 
+
+# #MEMBERSHIP OPS
+# #ASSIGNMENT OPS
+# #IDENTITY OPS
+# #LOGICAL OPS
+# #COMPARISM OPS
+
+# #ASSSIGNMENT OPERATORS 
+# my_name = "Inyang Paul" # string assingment
+
+# year = 2019 # integer assingment
+# print('before increment', year)
+
+# cart = ['shoes', 'socks', 'bags'] # list assingment
+
+# #increment operator
+# year += 1 #increase year by 1unit
+# print('after incremtn', year)
+# year += 5 #increase year by 1unit
+# print('after second incremtn by 5 ==>', year)
+
+
+# #decrement operator
+
+# year -= 1 #decreament year by 1unit
+# print('after decreament', year)
+# year -= 5 #increase year by 1unit
+# print('after second decreament by 5 ==>', year)
+
+# nums = ['a', 'b', 'c', 'd']
+# counter = 0
+
+# while counter < 4:
+#     char = nums[counter]
+#     print(counter, '. character : ', char)
+
+#     counter += 1
+
+# name = "John"
+# surname = "Doe"
+
+# full_name = "Mr " + name + " " + surname #Concatenation
+# print(full_name)
+# #and logic
+# # a   b   out
+# # T   T   T
+# # T   F   F
+
+# #STRING
+
+# my_string = 'hello world i am writing a string'
+# my_string2 = "hello world i am writing a string"
+# my_string3 = """hello world i am writing a string"""
+
+# print(my_string)
+# print(my_string2)
 # DATATYPES
 
 # INTEGERS
@@ -27,17 +153,117 @@
 # print(side_c)
 # print(rounded_result)
 
-print()
-print("Hello user please note that the")
-print("program outputs 0 for a leap ")
-print("year and any other number for a none leap year")
-print()
+# print()
+# print("Hello user please note that the")
+# print("program outputs 0 for a leap ")
+# print("year and any other number for a none leap year")
+# print()
 
-year = int(input("Please enter a year to test : "))
+# year = int(input("Please enter a year to test : "))
 
-remainder = year % 4
+# remainder = year % 4
 
-print(year, "--->", remainder)
-print(year, "is a Leap year --->", not bool(remainder))
+# print(year, "--->", remainder)
+# print(year, "is a Leap year --->", not bool(remainder))
 
 # WRITE A PROGRAM TO TAKE A PERSONS AGE, AND FIND THE CORRSPONDING YEAR OF BIRTH AND THEN TELL IF SAID PERSON WAS BORN IN A LEAP YEAR
+
+# comment (code ctrl + /)
+ 
+# age = int(input("Please enter your age : "))
+
+# year_of_birth = 2019 - age
+
+# remainder = year_of_birth % 4
+
+# print('Hello you were born in the year', year_of_birth)
+# print(year_of_birth, "divided by 4 --->", remainder)
+# print(year_of_birth, "is a Leap year --->", not bool(remainder))
+
+# OTHER OPERATORS
+
+#EXPONENT (**) RAISE TO POWER
+# X = 3 ** 3 # --> 27
+# print(X)
+# #FLOOR DIVISION (//) RAISE TO POWER
+# Y = 22 // 7 # --> 3
+# print(Y)
+
+# rounded = round(22/7, 4)#approximation using the round function
+# print(rounded)
+
+
+# #OPERATORS 
+
+# #MEMBERSHIP OPS
+# #ASSIGNMENT OPS
+# #IDENTITY OPS
+# #LOGICAL OPS
+# #COMPARISM OPS
+
+# #ASSSIGNMENT OPERATORS 
+# my_name = "Inyang Paul" # string assingment
+
+# year = 2019 # integer assingment
+# print('before increment', year)
+
+# cart = ['shoes', 'socks', 'bags'] # list assingment
+
+# #increment operator
+# year += 1 #increase year by 1unit
+# print('after incremtn', year)
+# year += 5 #increase year by 1unit
+# print('after second incremtn by 5 ==>', year)
+
+
+# #decrement operator
+
+# year -= 1 #decreament year by 1unit
+# print('after decreament', year)
+# year -= 5 #increase year by 1unit
+# print('after second decreament by 5 ==>', year)
+
+# nums = ['a', 'b', 'c', 'd']
+# counter = 0
+
+# while counter < 4:
+#     char = nums[counter]
+#     print(counter, '. character : ', char)
+
+#     counter += 1
+
+# name = "John"
+# surname = "Doe"
+
+# full_name = "Mr " + name + " " + surname #Concatenation
+# print(full_name)
+#and logic
+# a   b   out
+# T   T   T
+# T   F   F
+
+#STRING
+
+# my_string = 'hello world i am writing a string'
+# my_string2 = "hello world i am writing a string\n"
+
+# my_string3 = """hello world i am writing a string
+# and this one is multiline ,
+# cool right""" #ALSO KNOWN DOC STRING
+
+# print(my_string)
+# print(my_string2)
+# print(my_string3)
+
+
+#MEMBERSHIP OPS
+
+name = 'African elephant'
+
+search_char = 'european'
+
+value_in_name = search_char not in name
+
+print(search_char, 'in', name, value_in_name)
+
+#WRITE PROGRAM TO TEST FOR CHARACTERS A, X, N 
